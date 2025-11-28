e76f72f772b60e900e5227ccab2eb6e2a716556c
IamNeoleon <kusherbaev.bakhtiyar@gmail.com>
2025-11-28 12:53:34 +0500

    added adaptive for checkout

--- src/components/ProductsList/ProductsList.tsx
+++ src/components/ProductsList/ProductsList.tsx
@@ -7,32 +7,32 @@ import { useProducts } from '../../hooks/useProducts';
 
 
 export const ProductsList: React.FC = () => {
-	const { products, isLoading, isError, err } = useProducts()
-	console.log(products);
-	return (
-		<>
-			<div className='products__items' style={{ display: !isError && products.length ? 'grid' : 'block' }}>
-				{
-					isLoading ? (
-						[...new Array(12)].map((_, index) => <div key={`skeleton_${index}`}><CardLoader /></div>)
-					) : (
-						isError ? (
-							<div className='products__items-error'>
-								<h2>Error loading products</h2>
-								<p>{err}</p>
-							</div>
-						) : (
-							products.map((item, index) => (
-								<AnimatedCard key={item.id} index={index}>
-									<Link to={`/products/${item.id}`}>
-										<Card title={item.title} price={item.price} imageUrl={item.imageUrl} />
-									</Link>
-								</AnimatedCard>
-							))
-						)
-					)
-				}
-			</div>
-		</>
-	);
+   const { products, isLoading, isError, err } = useProducts()
+
+   return (
+      <>
+         <div className='products__items' style={{ display: !isError && products.length ? 'grid' : 'block' }}>
+            {
+               isLoading ? (
+                  [...new Array(12)].map((_, index) => <div key={`skeleton_${index}`}><CardLoader /></div>)
+               ) : (
+                  isError ? (
+                     <div className='products__items-error'>
+                        <h2>Error loading products</h2>
+                        <p>{err}</p>
+                     </div>
+                  ) : (
+                     products.map((item, index) => (
+                        <AnimatedCard key={item.id} index={index}>
+                           <Link to={`/products/${item.id}`}>
+                              <Card title={item.title} price={item.price} imageUrl={item.imageUrl} />
+                           </Link>
+                        </AnimatedCard>
+                     ))
+                  )
+               )
+            }
+         </div>
+      </>
+   );
 };
